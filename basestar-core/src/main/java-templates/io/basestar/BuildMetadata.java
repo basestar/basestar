7366aba6433887fbfaf1032b86fb18a6b4720ae6
mattevans <matt@arcneon.com>
2020-04-27 22:43:57 +0100

    Restore generated build metadata

---
+++ basestar-core/src/main/java-templates/io/basestar/BuildMetadata.java
@@ -0,0 +1,9 @@
+package io.basestar;
+
+public class BuildMetadata {
+
+    public static final String VERSION = "${project.version}";
+
+    public static final String TIMESTAMP = "${timestamp}";
+
+}
