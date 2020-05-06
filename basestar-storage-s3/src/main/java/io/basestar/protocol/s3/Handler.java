740e6675ae257ff8b6386d11f171470b2d1a8e80
mattevans <matt@arcneon.com>
2020-05-06 23:01:57 +0100

    Log on failing S3 protocol connect

--- basestar-storage-s3/src/main/java/io/basestar/protocol/s3/Handler.java
+++ basestar-storage-s3/src/main/java/io/basestar/protocol/s3/Handler.java
@@ -2,6 +2,7 @@ package io.basestar.protocol.s3;
 
 import com.google.common.collect.ImmutableList;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
@@ -17,6 +18,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
+@Slf4j
 @RequiredArgsConstructor
 @SuppressWarnings("unused")
 public class Handler extends URLStreamHandler {
@@ -57,18 +59,23 @@ public class Handler extends URLStreamHandler {
             @Override
             public void connect() {
 
-                final String bucket = stripLeadingSlashes(url.getHost());
-                final String key = stripLeadingSlashes(url.getPath());
+                try {
+                    final String bucket = stripLeadingSlashes(url.getHost());
+                    final String key = stripLeadingSlashes(url.getPath());
 
-                this.inputStream = client.getObject(GetObjectRequest.builder()
-                        .bucket(bucket).key(key).build());
+                    this.inputStream = client.getObject(GetObjectRequest.builder()
+                            .bucket(bucket).key(key).build());
 
-                final GetObjectResponse response = inputStream.response();
+                    final GetObjectResponse response = inputStream.response();
 
-                this.headers = new HashMap<>();
-                headers.put("content-length", ImmutableList.of(Long.toString(response.contentLength())));
-                headers.put("content-type", ImmutableList.of(response.contentType()));
-                headers.put("content-encoding", ImmutableList.of(response.contentEncoding()));
+                    this.headers = new HashMap<>();
+                    headers.put("content-length", ImmutableList.of(Long.toString(response.contentLength())));
+                    headers.put("content-type", ImmutableList.of(response.contentType()));
+                    headers.put("content-encoding", ImmutableList.of(response.contentEncoding()));
+                } catch (final Exception e) {
+                    log.error("Failed to connect to " + url);
+                    throw e;
+                }
             }
         };
     }
