4679a8d7e62c81a349940d6c5f0038d89084258f
Matt Evans <matt@arcneon.com>
2021-06-10 11:04:00 +0100

    Disable custom array handling in Trino dialect

--- basestar-storage-sql/src/main/java/io/basestar/storage/sql/dialect/TrinoDialect.java
+++ basestar-storage-sql/src/main/java/io/basestar/storage/sql/dialect/TrinoDialect.java
@@ -1,15 +1,9 @@
 package io.basestar.storage.sql.dialect;
 
-import io.basestar.schema.use.*;
-import io.basestar.util.Page;
-import org.jooq.Configuration;
+import io.basestar.schema.use.UseString;
 import org.jooq.DataType;
 import org.jooq.SQLDialect;
-import org.jooq.impl.DefaultDataType;
 import org.jooq.impl.SQLDataType;
-
-import java.util.*;
-import java.util.stream.Collectors;
 
 public class TrinoDialect extends JSONDialect {
 
@@ -55,94 +49,94 @@ public class TrinoDialect extends JSONDialect {
         return SQLDataType.VARCHAR;
     }
 
-    @Override
-    public <T> DataType<?> arrayType(final UseArray<T> type) {
+//    @Override
+//    public <T> DataType<?> arrayType(final UseArray<T> type) {
+//
+//        return dataType(type.getType()).getArrayDataType();
+//    }
+//
+//    @Override
+//    public <T> DataType<?> setType(final UseSet<T> type) {
+//
+//        return dataType(type.getType()).getArrayDataType();
+//    }
+//
+//    @Override
+//    public <T> DataType<?> pageType(final UsePage<T> type) {
+//
+//        return dataType(type.getType()).getArrayDataType();
+//    }
+//
+//    @Override
+//    public <T> DataType<?> mapType(final UseMap<T> type) {
+//
+//        return new MapDataType<>(dataType(type.getType()));
+//    }
+//
+//    @Override
+//    public <T> Object arrayToSQLValue(final UseArray<T> type, final List<T> value) {
+//
+//        return ((Collection<?>)value).stream().map(v -> toSQLValue(type.getType(), v)).toArray();
+//    }
+//
+//    @Override
+//    public <T> Object pageToSQLValue(final UsePage<T> type, final Page<T> value) {
+//
+//        return ((Collection<?>)value).stream().map(v -> toSQLValue(type.getType(), v)).toArray();
+//    }
+//
+//    @Override
+//    public <T> Object setToSQLValue(final UseSet<T> type, final Set<T> value) {
+//
+//        return ((Collection<?>)value).stream().map(v -> toSQLValue(type.getType(), v)).toArray();
+//    }
+//
+//    @Override
+//    public <T> List<T> arrayFromSQLValue(final UseArray<T> type, final Object value) {
+//
+//        return type.create(Arrays.stream((Object[]) value).map(v -> fromSQLValue(type.getType(), v)).collect(Collectors.toList()));
+//    }
+//
+//    @Override
+//    public <T> Page<T> pageFromSQLValue(final UsePage<T> type, final Object value) {
+//
+//        return type.create(Arrays.stream((Object[]) value).map(v -> fromSQLValue(type.getType(), v)).collect(Collectors.toList()));
+//    }
+//
+//    @Override
+//    public <T> Set<T> setFromSQLValue(final UseSet<T> type, final Object value) {
+//
+//        return type.create(Arrays.stream((Object[]) value).map(v -> fromSQLValue(type.getType(), v)).collect(Collectors.toSet()));
+//    }
 
-        return dataType(type.getType()).getArrayDataType();
-    }
-
-    @Override
-    public <T> DataType<?> setType(final UseSet<T> type) {
-
-        return dataType(type.getType()).getArrayDataType();
-    }
-
-    @Override
-    public <T> DataType<?> pageType(final UsePage<T> type) {
-
-        return dataType(type.getType()).getArrayDataType();
-    }
-
-    @Override
-    public <T> DataType<?> mapType(final UseMap<T> type) {
-
-        return new MapDataType<>(dataType(type.getType()));
-    }
-
-    @Override
-    public <T> Object arrayToSQLValue(final UseArray<T> type, final List<T> value) {
-
-        return ((Collection<?>)value).stream().map(v -> toSQLValue(type.getType(), v)).toArray();
-    }
-
-    @Override
-    public <T> Object pageToSQLValue(final UsePage<T> type, final Page<T> value) {
-
-        return ((Collection<?>)value).stream().map(v -> toSQLValue(type.getType(), v)).toArray();
-    }
-
-    @Override
-    public <T> Object setToSQLValue(final UseSet<T> type, final Set<T> value) {
-
-        return ((Collection<?>)value).stream().map(v -> toSQLValue(type.getType(), v)).toArray();
-    }
-
-    @Override
-    public <T> List<T> arrayFromSQLValue(final UseArray<T> type, final Object value) {
-
-        return type.create(Arrays.stream((Object[]) value).map(v -> fromSQLValue(type.getType(), v)).collect(Collectors.toList()));
-    }
-
-    @Override
-    public <T> Page<T> pageFromSQLValue(final UsePage<T> type, final Object value) {
-
-        return type.create(Arrays.stream((Object[]) value).map(v -> fromSQLValue(type.getType(), v)).collect(Collectors.toList()));
-    }
-
-    @Override
-    public <T> Set<T> setFromSQLValue(final UseSet<T> type, final Object value) {
-
-        return type.create(Arrays.stream((Object[]) value).map(v -> fromSQLValue(type.getType(), v)).collect(Collectors.toSet()));
-    }
-
-    static class MapDataType<T> extends DefaultDataType<Map<String, T>> {
-
-        final DataType<T> valueType;
-
-        @SuppressWarnings("unchecked")
-        public MapDataType(final DataType<T> valueType) {
-
-            super(null, (Class<Map<String, T>>)(Class<?>)Map.class, valueType.getTypeName());
-            this.valueType = valueType;
-        }
-
-        @Override
-        public final String getTypeName(Configuration configuration) {
-
-            final String typeName = this.valueType.getTypeName(configuration);
-            return getMapType(configuration, typeName);
-        }
-
-        @Override
-        public final String getCastTypeName(Configuration configuration) {
-
-            final String castTypeName = this.valueType.getCastTypeName(configuration);
-            return getMapType(configuration, castTypeName);
-        }
-
-        private static String getMapType(final Configuration configuration, final String dataType) {
-
-            return "map<varchar," + dataType + ">";
-        }
-    }
+//    static class MapDataType<T> extends DefaultDataType<Map<String, T>> {
+//
+//        final DataType<T> valueType;
+//
+//        @SuppressWarnings("unchecked")
+//        public MapDataType(final DataType<T> valueType) {
+//
+//            super(null, (Class<Map<String, T>>)(Class<?>)Map.class, valueType.getTypeName());
+//            this.valueType = valueType;
+//        }
+//
+//        @Override
+//        public final String getTypeName(Configuration configuration) {
+//
+//            final String typeName = this.valueType.getTypeName(configuration);
+//            return getMapType(configuration, typeName);
+//        }
+//
+//        @Override
+//        public final String getCastTypeName(Configuration configuration) {
+//
+//            final String castTypeName = this.valueType.getCastTypeName(configuration);
+//            return getMapType(configuration, castTypeName);
+//        }
+//
+//        private static String getMapType(final Configuration configuration, final String dataType) {
+//
+//            return "map<varchar," + dataType + ">";
+//        }
+//    }
 }
