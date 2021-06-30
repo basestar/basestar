21657fd20547c90a54ab286728b892933be2a2a3
Matt Evans <matt@arcneon.com>
2021-06-30 13:03:28 +0100

    Make all compatibility UDFs null safe

--- basestar-spark/src/main/java/io/basestar/spark/util/CompatibilityUDFs.java
+++ basestar-spark/src/main/java/io/basestar/spark/util/CompatibilityUDFs.java
@@ -135,21 +135,21 @@ public class CompatibilityUDFs {
         final UDFRegistration udfs = session.sqlContext().udf();
 
         udfs.register("date_add",
-                (UDF3<String, Long, Date, Timestamp>) (unit, amount, value) -> DateUnit.from(unit).add(amount, value),
+                (UDF3<String, Long, Date, Timestamp>) (unit, amount, value) -> (unit == null || amount == null || value == null) ? null : DateUnit.from(unit).add(amount, value),
                 DataTypes.TimestampType);
 
         udfs.register("date_diff",
-                (UDF3<String, Date, Date, Long>) (unit, a, b) -> DateUnit.from(unit).between(a, b), DataTypes.LongType);
+                (UDF3<String, Date, Date, Long>) (unit, a, b) -> (unit == null || a == null || b == null) ? null : DateUnit.from(unit).between(a, b), DataTypes.LongType);
 
         udfs.register("date_trunc",
-                (UDF2<String, Date, Timestamp>) (unit, value) -> DateUnit.from(unit).trunc(value), DataTypes.TimestampType);
+                (UDF2<String, Date, Timestamp>) (unit, value) -> (unit == null || value == null) ? null : DateUnit.from(unit).trunc(value), DataTypes.TimestampType);
 
         udfs.register("from_iso8601_date",
-                (UDF1<String, java.sql.Date>) str -> ISO8601.toSqlDate(ISO8601.parseDate(str)),
+                (UDF1<String, java.sql.Date>) str -> (str == null) ? null : ISO8601.toSqlDate(ISO8601.parseDate(str)),
                 DataTypes.DateType);
 
         udfs.register("from_iso8601_timestamp",
-                (UDF1<String, Timestamp>) str -> ISO8601.toSqlTimestamp(ISO8601.parseDateTime(str)),
+                (UDF1<String, Timestamp>) str -> (str == null) ? null : ISO8601.toSqlTimestamp(ISO8601.parseDateTime(str)),
                 DataTypes.TimestampType);
     }
 
