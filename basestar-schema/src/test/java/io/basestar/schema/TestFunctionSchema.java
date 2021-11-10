00cd2eeefb156bc4f1c17f5f474961e1ca2cb36d
DonatasMockus <63247190+DonatasMockus@users.noreply.github.com>
2021-11-10 16:31:19 +0200

    DPT-796  Improve schema dependencies replacement (#136)
    * DPT-796  Improve schema dependencies replacement

--- basestar-schema/src/test/java/io/basestar/schema/TestFunctionSchema.java
+++ basestar-schema/src/test/java/io/basestar/schema/TestFunctionSchema.java
@@ -5,10 +5,16 @@ import com.google.common.collect.ImmutableSet;
 import io.basestar.schema.from.FromSchema;
 import io.basestar.util.Name;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestFunctionSchema {
+
 
     @Test
     public void testReplacedDefinition() {
@@ -21,14 +27,100 @@ public class TestFunctionSchema {
         assertEquals("SELECT * FROM test.Object", output);
     }
 
-    @Test
-    public void testReplacedDefinitionQuoted() {
+    @ParameterizedTest(name = "{index}: replace\"{1}\", in \"{0}\"")
+    @MethodSource("dotsReplacements")
+    public void shouldReplaceDotsInProvidedSearchTerm(final String input, final String searchTerm, final String expected) {
+        //if dots not replaced it can match any char i.e. DOG,CONTEST would match for DOG.CONTEST
+        final String output = FunctionSchema.getReplacedDefinition(
+                input,
+                ImmutableMap.of(searchTerm, new FromSchema(ObjectSchema.builder().build(Name.of("dog", "contestWinners")), ImmutableSet.of())),
+                v -> v.getQualifiedName().toString());
 
-        final String input = "SELECT * FROM \"@{test}\"";
-        final String output = FunctionSchema.getReplacedDefinition(input, ImmutableMap.of("test", new FromSchema(
+        assertEquals(expected, output);
+    }
+
+    private static Stream<Arguments> dotsReplacements() {
+        return Stream.of(
+                Arguments.of("SELECT DOG,CONTEST FROM DOG.CONTEST", "DOG.CONTEST", "SELECT DOG,CONTEST FROM dog.contestWinners"),
+                Arguments.of("SELECT DOG,CONTEST FROM \"DOG\".\"CONTEST\"", "\"DOG\".\"CONTEST\"", "SELECT DOG,CONTEST FROM dog.contestWinners"),
+                Arguments.of("SELECT DOG_CONTEST FROM \"DOG\".\"CONTEST\"", "\"DOG\".\"CONTEST\"", "SELECT DOG_CONTEST FROM dog.contestWinners")
+        );
+    }
+
+    @ParameterizedTest(name = "{index}: replace\"{1}\", in \"{0}\"")
+    @MethodSource("replacementAtTheStart")
+    public void shouldReplaceIfSearchTermIsAtTheStartOfGivenString(final String input, final String searchTerm, final String expected) {
+
+        final String output = FunctionSchema.getReplacedDefinition(
+                input,
+                ImmutableMap.of(searchTerm, new FromSchema(ObjectSchema.builder().build(Name.of("dog", "contest")), ImmutableSet.of())),
+                v -> v.getQualifiedName().toString());
+
+        assertEquals(expected, output);
+    }
+
+    private static Stream<Arguments> replacementAtTheStart() {
+        return Stream.of(
+                Arguments.of("DOG_CONTEST(ARRAY_TO_STRING(ARRAY_AGG(DISTINCT  MVW.SOURCE), ',')) AS SCORE", "DOG_CONTEST", "dog.contest(ARRAY_TO_STRING(ARRAY_AGG(DISTINCT  MVW.SOURCE), ',')) AS SCORE"),
+                Arguments.of(" DOG_CONTEST(ARRAY_TO_STRING(ARRAY_AGG(DISTINCT  MVW.SOURCE), ',')) AS SCORE", "DOG_CONTEST", " dog.contest(ARRAY_TO_STRING(ARRAY_AGG(DISTINCT  MVW.SOURCE), ',')) AS SCORE"),
+                Arguments.of("\"DOG_CONTEST\"(ARRAY_TO_STRING(ARRAY_AGG(DISTINCT  MVW.SOURCE), ',')) AS SCORE", "\"DOG_CONTEST\"", "dog.contest(ARRAY_TO_STRING(ARRAY_AGG(DISTINCT  MVW.SOURCE), ',')) AS SCORE"),
+                Arguments.of("\"DOG\".\"CONTEST\"(ARRAY_TO_STRING(ARRAY_AGG(DISTINCT  MVW.SOURCE), ',')) AS SCORE", "\"DOG\".\"CONTEST\"", "dog.contest(ARRAY_TO_STRING(ARRAY_AGG(DISTINCT  MVW.SOURCE), ',')) AS SCORE"),
+                Arguments.of("\"DOG\".\"CONTEST\" (ARRAY_TO_STRING(ARRAY_AGG(DISTINCT  MVW.SOURCE), ',')) AS SCORE", "\"DOG\".\"CONTEST\"", "dog.contest (ARRAY_TO_STRING(ARRAY_AGG(DISTINCT  MVW.SOURCE), ',')) AS SCORE")
+        );
+    }
+
+    @ParameterizedTest(name = "{index}: replace\"{1}\", in \"{0}\"")
+    @MethodSource("replacementAtTheEnd")
+    public void shouldReplaceIfSearchTermIsAtTheEndOfGivenString(final String input, final String searchTerm, final String expected) {
+
+
+        final String output = FunctionSchema.getReplacedDefinition(input, ImmutableMap.of(searchTerm, new FromSchema(
                 ObjectSchema.builder().build(Name.of("test", "Object")), ImmutableSet.of()
         )), v -> v.getQualifiedName().toString());
 
-        assertEquals("SELECT * FROM test.Object", output);
+        assertEquals(expected, output);
+    }
+
+    private static Stream<Arguments> replacementAtTheEnd() {
+        return Stream.of(
+                Arguments.of("SELECT * FROM test", "test", "SELECT * FROM test.Object"),
+                Arguments.of("SELECT * FROM test.targets", "test.targets", "SELECT * FROM test.Object"),
+                Arguments.of("SELECT * FROM \"test\".\"targets\"", "\"test\".\"targets\"", "SELECT * FROM test.Object"),
+                Arguments.of("SELECT * FROM test_targets", "test_targets", "SELECT * FROM test.Object")
+        );
+    }
+
+    @ParameterizedTest(name = "{index}: replace\"{1}\", in \"{0}\"")
+    @MethodSource("replacementInTheMiddle")
+    public void shouldReplaceIfSearchTermIsInTheMiddle(final String input, final String searchTerm, final String expected) {
+
+        final String output = FunctionSchema.getReplacedDefinition(input, ImmutableMap.of(searchTerm, new FromSchema(
+                ObjectSchema.builder().build(Name.of("test", "Object")), ImmutableSet.of()
+        )), v -> v.getQualifiedName().toString());
+
+        assertEquals(expected, output);
+    }
+
+    private static Stream<Arguments> replacementInTheMiddle() {
+        return Stream.of(
+                Arguments.of("SELECT * FROM test where result is null", "test", "SELECT * FROM test.Object where result is null"),
+                Arguments.of("SELECT * FROM test_runs where result is null", "test_runs", "SELECT * FROM test.Object where result is null"),
+                Arguments.of("SELECT * FROM test.runs where result is null", "test.runs", "SELECT * FROM test.Object where result is null"),
+                Arguments.of("SELECT * FROM \"test\".\"runs\" where result is null", "\"test\".\"runs\"", "SELECT * FROM test.Object where result is null")
+        );
+    }
+
+    @Test
+    public void shouldReplaceIfMultipleDifferentMatches() {
+
+        final String input = "SELECT * FROM test where result is null JOIN \"badTestRuns\" btr ON btr.id = id JOIN @{example_test_runs} on id";
+        final String output = FunctionSchema.getReplacedDefinition(input,
+                ImmutableMap.of("test", new FromSchema(ObjectSchema.builder().build(Name.of("test", "Object")), ImmutableSet.of()),
+                        "\"badTestRuns\"", new FromSchema(ObjectSchema.builder().build(Name.of("bad", "test", "runs")), ImmutableSet.of()),
+                        "example_test_runs", new FromSchema(ObjectSchema.builder().build(Name.of("example_test_runs")), ImmutableSet.of())
+                ),
+                v -> v.getQualifiedName().toString());
+
+        assertEquals("SELECT * FROM test.Object where result is null JOIN bad.test.runs btr ON btr.id = id JOIN example_test_runs on id", output);
     }
 }
