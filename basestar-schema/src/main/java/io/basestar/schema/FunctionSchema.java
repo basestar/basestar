d551423de28a3d4a3ece4754c4979e36c7960d98
Matt Evans <matt@arcneon.com>
2022-02-02 15:32:29 +0000

    DPT-1028 Make sure descriptors are public

--- basestar-schema/src/main/java/io/basestar/schema/FunctionSchema.java
+++ basestar-schema/src/main/java/io/basestar/schema/FunctionSchema.java
@@ -70,6 +70,11 @@ public class FunctionSchema implements Schema {
     @Nonnull
     private final Map<String, From> using;
 
+    public static Builder builder() {
+
+        return new Builder();
+    }
+
     public FunctionSchema(final Descriptor descriptor, final Resolver.Constructing resolver, final Version version, @Nonnull final Name qualifiedName, final int slot) {
 
         resolver.constructing(qualifiedName, this);
